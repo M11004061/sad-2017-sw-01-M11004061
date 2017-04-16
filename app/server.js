669780cb1d1829bcbbb8e0d661319eaa0b07fe46
M11004061 <marcus.ritter@fh-heidelberg.de>
2017-04-16 23:03:15 +0200

    added basic server code

--- app/server.js
+++ app/server.js
@@ -1,14 +1,29 @@
-const http = require('http');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var index = require('./routes/index');
+var restaurants = require('./routes/restaurants');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+var port = 3000;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+//Body Parser MW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api', restaurants);
+
+app.listen(port, function(){
+	console.log('Server started on port '+port);
 });
