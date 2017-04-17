5ff46856906184ab6ea91615370330d74152819a
M11004061 <marcus.ritter@fh-heidelberg.de>
2017-04-17 16:32:39 +0200

    installed bootstrap

---
+++ app/client/src/app/components/restaurants/restaurants.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RestaurantService} from '../../services/restaurant.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'restaurants',
+  templateUrl: 'restaurants.component.html',
+})
+export class RestaurantsComponent  { 
+    constructor(private restaurantService:RestaurantService) {
+        this.restaurantService.getRestaurants()
+            .subscribe(restaurants => {
+                console.log(restaurants);
+            });
+    }   
+}
---
+++ app/client/src/app/services/restaurant.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class RestaurantService{
+    constructor(private http:Http){
+        console.log('Task Service Initialized...');
+    }
+    
+    getRestaurants(){
+        return this.http.get('http://localhost:3000/api/restaurants')
+            .map(res => res.json());
+    }
+}
+
