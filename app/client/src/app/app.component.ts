b2929c1d8bb6af33ea4b981898d9da8db8166a11
M11004061 <marcus.ritter@fh-heidelberg.de>
2017-04-17 16:31:43 +0200

    updated app components

--- app/client/src/app/app.component.ts
+++ app/client/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RestaurantService } from './services/restaurant.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app.component.html',
+  providers:[RestaurantService]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { }
--- app/client/src/app/app.module.ts
+++ app/client/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent }  from './app.component';
+import { RestaurantsComponent }  from './components/restaurants/restaurants.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, HttpModule ],
+  declarations: [ AppComponent, RestaurantsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
